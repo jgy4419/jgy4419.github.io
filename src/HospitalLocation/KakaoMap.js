64f30f8146828e37d1f48f14b7634880bb9fe8e3
jgy4419 <jgy_98@naver.com>
2022-04-13 22:49:39 +0900

    .

--- src/HospitalLocation/KakaoMap.js
+++ src/HospitalLocation/KakaoMap.js
@@ -29,6 +29,8 @@ function KakaoMap(){
         title: [] // 병원 이름
     }
 
+    let [changeInfoDiv, setChangeInfoDiv] = useState('');
+
     useEffect(async () => {
         // 처음에 지도 표시해주기.
         mapContainer = document.getElementById('map'); // 지도를 표시할 div 
@@ -68,6 +70,58 @@ function KakaoMap(){
                     dispatch({type: '내위치', payload: {x: latitude, y: longitude}});
                     // 위치를 찾으면 카카오맵의 위치를 현재위치로 재설정해서 재로딩 시켜줌.
                     resolve(mapReset(state[0].clickLocationsX, state[0].clickLocationsY));
+                    var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+                    
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };  
+    console.log(changeInfoDiv);
+// 지도를 생성합니다    
+// var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places(); 
+
+// 키워드로 장소를 검색합니다
+ps.keywordSearch(`${changeInfoDiv} 병원`, placesSearchCB); 
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);    
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }       
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    } 
+}
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+    
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x) 
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+    });
+}
                 }, function(error){
                     console.log(error);
                 }, {
@@ -81,6 +135,10 @@ function KakaoMap(){
             }
             // allLocation();
         })
+        // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+        kakao.maps.event.addListener(map, 'idle', function() {
+            searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+        });
     }, [])
     // 사이트가 재로딩되면서 내 위치를 새로 잡아줌.
     function reload(){
@@ -95,7 +153,8 @@ function KakaoMap(){
             center: new kakao.maps.LatLng(state[0].clickLocationsX, state[0].clickLocationsY), // 지도의 중심좌표
             level: 3 // 지도의 확대 레벨
         };
-        map = new kakao.maps.Map(mapContainer, mapOption);   
+        map = new kakao.maps.Map(mapContainer, mapOption);
+
 
         // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능.
         let content = '<div class="customoverlay">' +
@@ -177,56 +236,57 @@ function KakaoMap(){
         searchAddrFromCoords(map.getCenter(), displayCenterInfo);
 
         // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
-        kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
-            searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
-                if (status === kakao.maps.services.Status.OK) {
-                    var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
-                    detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
+        // kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+        //     searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
+        //         if (status === kakao.maps.services.Status.OK) {
+        //             var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
+        //             detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
                     
-                    var content = '<div class="bAddr">' +
-                                    '<span class="title">법정동 주소정보</span>' + 
-                                    detailAddr + 
-                                '</div>';
+        //             var content = '<div class="bAddr">' +
+        //                             '<span class="title">법정동 주소정보</span>' + 
+        //                             detailAddr + 
+        //                         '</div>';
 
-                    // 마커를 클릭한 위치에 표시합니다 
-                    marker.setPosition(mouseEvent.latLng);
-                    marker.setMap(map);
+        //             // 마커를 클릭한 위치에 표시합니다 
+        //             marker.setPosition(mouseEvent.latLng);
+        //             marker.setMap(map);
 
-                    // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
-                    infowindow.setContent(content);
-                    infowindow.open(map, marker);
-                }   
-            });
-        });
+        //             // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
+        //             infowindow.setContent(content);
+        //             infowindow.open(map, marker);
+        //         }   
+        //     });
+        // });
 
-        // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
-        kakao.maps.event.addListener(map, 'idle', function() {
-            searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-        });
+        // // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+        // kakao.maps.event.addListener(map, 'idle', function() {
+        //     searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+        // });
 
         function searchAddrFromCoords(coords, callback) {
             // 좌표로 행정동 주소 정보를 요청합니다
             geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);         
         }
-
-        function searchDetailAddrFromCoords(coords, callback) {
-            // 좌표로 법정동 상세 주소 정보를 요청합니다
-            geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
-        }
-
-        // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
-        function displayCenterInfo(result, status) {
-            if (status === kakao.maps.services.Status.OK) {
-                var infoDiv = document.getElementById('centerAddr');
-
-                for(var i = 0; i < result.length; i++) {
-                    // 행정동의 region_type 값은 'H' 이므로
-                    if (result[i].region_type === 'H') {
-                        infoDiv.innerHTML = result[i].address_name;
-                        break;
-                    }
+        // function searchDetailAddrFromCoords(coords, callback) {
+        //     // 좌표로 법정동 상세 주소 정보를 요청합니다
+        //     geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+        // }
+    }
+    // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+    function displayCenterInfo(result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+            // infoDiv의 innerHTML 값을 전송
+            var infoDiv = document.getElementById('centerAddr');
+            for(var i = 0; i < result.length; i++) {
+                // 행정동의 region_type 값은 'H' 이므로
+                if (result[i].region_type === 'H') {
+                    infoDiv.innerHTML = result[i].address_name;
+                    break;
                 }
-            }    
+            }
+            changeInfoDiv = infoDiv.innerHTML;
+            console.log(changeInfoDiv);
+            
         }
     }
     return(
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -17,28 +17,21 @@ function Main(){
       }, 1000);
     }
     return(
-        // <div className="mainContainer">
-        //     <br/>
-        //     <div className="inner">
-        //         <p className="title">병원을 찾는 가장 빠른 방법</p>
-        //         <Link to="/hospital"><button className="btn">찾으러 가기</button></Link>
-        //     </div>
-        // </div>
         <div className='mainContainer'>
             <div className='inner'>
             <p className="title">병원을 찾는 <br/> 가장 빠른 방법.</p>
                 <div className='contain'>
                     <div className='item'>
-                        <p className='title1'>어떤 병원을 <br/>찾으세요?</p>
+                        <p className='title1'>어떤 <i className="fa fas fa-hospital"/>을 <br/>찾으세요?</p>
                         <Link to="/hospital"><button className="btn">찾으러 가기</button></Link>
                     </div>
                     <div className='item'>
                         <p className='title2'>내 위치 주변에 있는 <br/> 병원을 찾아봐요!</p>
-                        <i className="fa fas fa-hospital"></i>
+                        <i className="fa fas fa-location-arrow"></i>
                     </div>
                     <div className='item'>
-                        <p className='title3'>개선할 점을 말해주세요!</p>
-                        <Link to="/hospital"><button className="btnAbout">문의하기</button></Link>
+                        <p className='title3'>개선할 점을 말해주세요!👨‍💻🧑🏻‍💻👩🏻‍💻</p>
+                        <Link to="/about"><button className="btnAbout">문의하기</button></Link>
                     </div>
                     </div>
             </div>
